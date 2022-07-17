1528f7cb422eed93f7d7efa9d881284596e9e172
Abhinav Raju <abhinavraju@Abhinavs-MacBook-Pro.local>
2022-07-16 20:36:49 -0700

    Added html css and js file

---
+++ script.js
@@ -0,0 +1,16 @@
+function Calculate(){
+    let np= document.getElementById("number").value;
+    let bill= document.getElementById("bill").value;
+    let tipper= document.getElementById("tip%").value;
+
+    let tippercent= bill * tipper / 100;
+    let final_tip = tippercent/np;
+
+    //document.getElementById("tip").style.display = "block";
+    document.getElementById("total").innerHTML = final_tip;
+}
+
+document.getElementById("button").onclick = function (){
+    //document.getElementById("total").innerHTML = "hello";
+    Calculate();
+};
