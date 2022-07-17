1331edcf2a3404793a0105a986155be53227dfc6
Abhinav Raju <abhinavraju@Abhinavs-MacBook-Pro.local>
2022-07-17 16:45:45 -0700

    added div elements from the script file

--- script.js
+++ script.js
@@ -5,11 +5,28 @@ function Calculate(){
 
     let tippercent= bill * tipper / 100;
     let final_tip = tippercent/np;
+    let total_bill = bill + (bill * tipper /100);
 
     //document.getElementById("tip").style.display = "block";
-    document.getElementById("total").innerHTML = final_tip;
+     
+    // newdiv.setAttribute('id', 'output');
+    //document.getElementById("tip").style.display = "none";
+     const maindiv= document.getElementById('tip');
+     const newdiv = document.createElement('div');
+     
+     newdiv.setAttribute('id', 'output');
+     newdiv.innerHTML = "<p>Total Bill Each<p> ";
+     
+     maindiv.append(newdiv);
+     const span = document.createElement('span');
+     newdiv.append(span);
+     span.setAttribute('id','totalbill');
+     document.getElementById('totalbill').innerText = total_bill/2;
+    document.getElementById("total").innerHTML = final_tip.toFixed(2);
 }
-//document.getElementById("tip").style.display = "none";
+
+
+
 document.getElementById("button").onclick = function (){
     //document.getElementById("total").innerHTML = "hello";
     Calculate();
