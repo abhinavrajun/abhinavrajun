30fcebc6dd22338e10bc433723f5ae5a7efac540
Abhinav Raju <abhinavraju@Abhinavs-MacBook-Pro.local>
2022-07-18 21:14:53 -0700

    fixed bug for total bill and displaying div

--- script.js
+++ script.js
@@ -1,31 +1,23 @@
 function Calculate(){
     let np= document.getElementById("number").value;
     let bill= document.getElementById("bill").value;
-    let tipper= document.getElementById("tip%").value;
+    let tippercent= document.getElementById("tip%").value;
 
-    let tippercent= bill * tipper / 100;
-    let final_tip = tippercent/np;
-    let total_bill = bill + (bill * tipper /100);
+    let tip= bill * tippercent / 100;
+    let final_tip = tip/np;
+    let total_bill = (bill/np) + final_tip;
 
     //document.getElementById("tip").style.display = "block";
      
     // newdiv.setAttribute('id', 'output');
     //document.getElementById("tip").style.display = "none";
-     const maindiv= document.getElementById('tip');
-     const newdiv = document.createElement('div');
-     
-     newdiv.setAttribute('id', 'output');
-     newdiv.innerHTML = "<p>Total Bill Each<p> ";
-     
-     maindiv.append(newdiv);
-     const span = document.createElement('span');
-     newdiv.append(span);
-     span.setAttribute('id','totalbill');
-     document.getElementById('totalbill').innerText = total_bill/2;
+
+     document.getElementById("tip").style.display = "flex";
+     document.getElementById('total_bill').innerText = total_bill.toFixed(2);
     document.getElementById("total").innerHTML = final_tip.toFixed(2);
 }
 
-
+document.getElementById('tip').style.display = 'none'
 
 document.getElementById("button").onclick = function (){
     //document.getElementById("total").innerHTML = "hello";
